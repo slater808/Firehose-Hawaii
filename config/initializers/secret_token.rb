3a35054ae00f3d05e1edcd11f4d4ac0c62698074
Erik <erik@erik-VirtualBox.(none)>
2014-01-04 02:36:33 -1000

    building out our firehose app landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firehoseapp::Application.config.secret_key_base = 'fc50c4e9a0b47ddadd7116d66775a540ac62821693e6b9279434aee08ef0a15ffaf97bdc9f9d8c456656f30dbd1a0674bc93a80379415c45cda13a5dd227ada1'
